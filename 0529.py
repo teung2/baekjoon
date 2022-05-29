c901579df9671368ad98e81e41ca6d2eb555ba4e
SEUNGJOO PAEK <seungjoopaek@SEUNGJOOui-MacBookAir.local>
2022-05-29 23:22:57 +0900

    first commit

---
+++ 0529.py
@@ -0,0 +1,12 @@
+nat_num = set(range(1,10001))
+gen_num = set()
+
+for i in range(1,10001):
+    for j in str(i):
+        i += int(j)
+    gen_num.add(i)
+        
+self_num = sorted(set(nat_num - gen_num))
+for i in self_num:
+    print(i)
+
---
+++ caculate.py
@@ -0,0 +1,8 @@
+A = '7'
+B= '3'
+print(int(A)+int(B))
+print(int(A)-int(B))
+print(int(A)*int(B))
+print(int(A)//int(B))
+print(int(A)%int(B))
+print(2541-1998)
